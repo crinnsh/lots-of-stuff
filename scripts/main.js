d4f1428d88faefc208f9c9143914caccc6902172
crinnsh <117044606+crinnsh@users.noreply.github.com>
2022-11-23 21:24:32 +0100

    calculator

--- scripts/main.js
+++ scripts/main.js
@@ -44,3 +44,42 @@ toggle.addEventListener('click', () => {
 
 /* SECOND DIV */
 
+let screen = document.getElementById("calculator-screen");
+
+let calcBtns = Array.from(document.getElementsByClassName("calcBtn"));
+
+calcBtns.map(button => {
+    button.addEventListener("click", (b) => {
+        if (screen.innerText == "Noooooooo!") {
+            screen.innerText = "";
+        }
+
+        switch (b.target.innerText) {
+            case "CA":
+                screen.innerText = "";
+                break;
+
+            case "←":
+                if (screen.innerText) {
+                    screen.innerText = screen.innerText.slice(0, -1);
+                }
+                break;
+
+            case "=":
+                try {
+                    /* screen.innerText = eval(screen.innerText); */
+                    result = eval(screen.innerText);
+                    screen.innerText = Math.floor(result * 1000) / 1000;
+                } catch {
+                    screen.innerText = "Noooooooo!"
+                }
+                break;
+
+            default:
+                screen.innerText += b.target.innerText;
+        }
+    });
+});
+
+/* THIRD DIV */
+
